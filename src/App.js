6fafdef647c28f2bed5c99b8bd705a1884ffc644
Ashok Amara <109223439+ashoksrt-248@users.noreply.github.com>
2023-08-08 20:32:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import DataTable from "react-data-table-component";
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+import { columns, data } from "./data";
+
+function App() {
+  const tableData = {
+    columns,
+    data
+  };
+
+  return (
+    <div className="App">
+      <h1>React Data Table</h1>
+
+      <DataTableExtensions {...tableData}>
+        <DataTable
+          columns={columns}
+          data={data}
+          pagination
+          exportHeaders
+          responsive
+          highlightOnHover
+        />
+      </DataTableExtensions>
+    </div>
+  );
+}
+
+export default App;
